86467c393d91b8940716b847aa5172bc49766dc4
ayaam <ayaam.mit@gmail.com>
2024-12-24 00:16:05 +0530

    the divs are now beside each other

--- join_clarity/src/App.js
+++ join_clarity/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="parent">
+      <div className = "left">
+        <h1>Welcome to our community</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Clarity gives you the blocks and components you need to create a truly
+          professional website
+        </p> 
+        <p>
+          "We love Landingfolio! Our designers were using it for their projects,
+          so we already knew what kind of design they want."
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>Devon Lane (Co-Founder, Design.co)</p>
+      </div>
+      <div className = "right">
+        <h1>Join Clarity</h1>
+        <p>
+          Clarity gives you the blocks and components you need to create a truly
+          professional website
+        </p>
+        <p>Full Name</p>
+        {/* <input>Full Name</input> */}
+        <p>Email Address</p>
+        {/* <input>Email Address</input> */}
+        <p>Password</p>
+        {/* <input>Password</input> */}
+        <button>Remember Me</button>
+        <span>Forgot Password</span>
+        <button>Sign Up</button>
+        <span>Don't have an account ? Create free account</span>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
